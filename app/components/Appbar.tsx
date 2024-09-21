cf6232f8423244ee3ad763912926886d5771fa42
YashVentures <bhadauriyayash712@gmail.com>
2024-09-21 10:57:22 +0530

    google auth

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { signIn } from "next-auth/react"
+export function Appbar (){
+    return <div>
+        <div  className="flex justify-between">
+            <div>
+            muzi
+            </div>
+        <div>
+            <button className="m-2 p-2 bg-blue-400" onClick={()=> signIn()}>SignIn</button>
+        </div>
+        </div>
+       
+    </div>
+}
